5b1aa6a8a83c5be4b781e29ef7beb75f2177a642
ABulger01 <Nyesha2001@gmail.com>
2025-03-08 04:43:01 +0000

    added cin.get
    allowing for student to answer

--- main.cpp
+++ main.cpp
@@ -9,19 +9,21 @@ int main() {
   std::uniform_int_distribution<> dis (100,999);
   int randomNumber = dis(gen);
   int randomNumber2 = dis(gen);
+  
   // Step 2: DIsplay the two random numbers to the user
+  cout << "Hello, Welcome to Math Tutor 101: Solve the following problem\n";
   cout << "Random Number 1: " << randomNumber << endl;
   cout << "Random Number 2: " << randomNumber2 << endl;
   // Step 3: Ask the user to enter the addition of the two numbers
   cout << randomNumber << " + " << randomNumber2 << " = ? " << endl;
   // Step 4: Pause the program and wait for the user to enter the answer
   int userAnswer;
-  cin >> userAnswer;
-  cin.get();
+  cin >> userAnswer; 
+  cin.get(); // wait for the user to press enter
   
 // Step 4: pause to allow the user to enter the answer
-  cout << "Press Enter when you are ready to check your answer" << endl;
-  cin.get(); // wait for the user to press enter
+  cout << "Press Enter when you are ready to check your answer\n";
+  cin.get(); //wait for the user to press enter
   
   // Step 5: Display the correct answer to the user
   cout << "The correct answer is: " << randomNumber + randomNumber2 << endl;
