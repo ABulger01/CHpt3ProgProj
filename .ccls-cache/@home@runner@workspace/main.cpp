b7914b460c2ce59d53accba44a706fb7d35eef23
ABulger01 <Nyesha2001@gmail.com>
2025-03-08 02:42:02 +0000

    prompted the program to ask the user how many cookies are being made

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main()  { 
 //  Define default ingredients to make 48 cookies per batch
+const double sugar = 1.5;
+  const double butter = 1.0;
+  const double flour = 2.75;
+  const int cookies = 48;
 
-  const double cupsof
+  // Ask user for number of cookies to make
+  
